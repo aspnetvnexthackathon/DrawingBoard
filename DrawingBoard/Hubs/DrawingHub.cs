aad4387f15077f61cb4038109a37a0369a4bd2ba
Stephen Halter <halter73@gmail.com>
2014-06-25 16:06:09 -0700

    Added features to DrawingHub
    - Randomly select animal and then verify guess
    - Timeout a game after 60 seconds
    - And more!

--- DrawingBoard/Hubs/DrawingHub.cs
+++ DrawingBoard/Hubs/DrawingHub.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
 
 namespace DrawingBoard.Hubs
 {
     public class DrawinghHub : Hub
     {
-        private static Random _random;
+        private static readonly Random _random;
+
+        private static readonly ConcurrentDictionary<string, GameState> _games;
+        private static readonly GameState _newGame;
+
+        private static readonly Dictionary<string, string[]> _animalSynonyms;
+        private static readonly string[] _animals;
 
         static DrawinghHub()
         {
             _random = new Random();
+            _games = new ConcurrentDictionary<string, GameState>();
+            _newGame = new GameState();
+
+            // Generated from http://en.wikipedia.org/wiki/List_of_English_animal_nouns
+            var animalsJson = File.ReadAllText(Startup.AppBasePath + @"\Hubs\animals.json");
+            _animalSynonyms = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(animalsJson);
+            _animals = _animalSynonyms.Keys.ToArray();
         }
 
         #region Data structures
@@ -35,23 +53,74 @@ namespace DrawingBoard.Hubs
             "red", "green", "blue", "orange", "navy", "silver", "black", "lime"
         };
 
-        public async Task<int> CreateGame()
+        public async Task<string> CreateGame()
         {
-            int gameId = _random.Next();
+            var gameId = _random.Next().ToString();
+
+            if (!_games.TryAdd(gameId, _newGame))
+            {
+                throw new InvalidOperationException("Insufficient Entropy");
+            }
+
             Clients.Caller.color = colors[Interlocked.Increment(ref _id) % colors.Length];
-            await Groups.Add(Context.ConnectionId, gameId.ToString());
+            await Groups.Add(Context.ConnectionId, gameId);
+            await Groups.Add(Context.ConnectionId, "draw-" + gameId);
             return gameId;
         }
 
-        public async Task Join(int gameId)
+        public async Task Join(string gameId)
         {
+            GameState ignore;
+            if (!_games.TryGetValue(gameId, out ignore))
+            {
+                throw new InvalidOperationException("A game with the specified id has not yet been created.");
+            }
+
             Clients.Caller.color = colors[Interlocked.Increment(ref _id) % colors.Length];
-            await Groups.Add(Context.ConnectionId, gameId.ToString());
+            await Groups.Add(Context.ConnectionId, gameId);
+            await Groups.Add(Context.ConnectionId, "guess-" + gameId);
+            await Clients.Group("draw-" + gameId).playerJoined(gameId);
+        }
+
+        public string Start(string gameId)
+        {
+            var animal = _animals[_random.Next(_animals.Length)];
+            Timer gameTimer = null;
+            var gameState = new GameState
+            {
+                Animal = animal,
+                Timer = gameTimer
+            };
+
+            if (!_games.TryUpdate(gameId, gameState , _newGame))
+            {
+                throw new InvalidOperationException("A game with the specified id has alread started.");
+            }
+
+
+            gameTimer = new Timer(_ =>
+            {
+                Clients.Group(gameId).gameTimedOut(gameId);
+
+                gameTimer.Dispose();
+
+                GameState ignore;
+                _games.TryRemove(gameId, out ignore);
+            }, null, 60 * 1000, -1);;
+
+            return animal;
+        }
+
+        public bool Guess(string gameId, string animalGuess)
+        {
+            GameState game;
+            return _games.TryGetValue(gameId, out game) &&
+                (animalGuess == game.Animal || _animalSynonyms[game.Animal].Contains(animalGuess));
         }
 
         public void Draw(int gameId, Line data)
         {
-            Clients.OthersInGroup(gameId.ToString()).draw(data);
+            Clients.OthersInGroup(gameId.ToString()).draw(gameId, data);
         }
     }
 }
---
+++ DrawingBoard/Hubs/GameState.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+
+namespace DrawingBoard.Hubs
+{
+    public class GameState
+    {
+        public string Animal { get; set; }
+        public Timer Timer { get; set; }
+    }
+}
